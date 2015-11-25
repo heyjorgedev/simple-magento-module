37095a82a48c99208d0b85775b389fd0e53f6807
Jorge Murta <jmurta.jm@gmail.com>
2015-11-25 15:27:15 +0000

    Create Action Routes

--- app/code/local/JorgeMurta/SimpleModule/controllers/SimpleModuleController.php
+++ app/code/local/JorgeMurta/SimpleModule/controllers/SimpleModuleController.php
@@ -1,10 +1,18 @@
 <?php
 
-class JorgeMurta_SimpleModule_SimpleModuleController extends Mage_Adminhtml_Controller_Action
+class JorgeMurta_SimpleModule_SimplemoduleController extends Mage_Adminhtml_Controller_Action
 {
     public function indexAction()
     {
         $this->loadLayout();
+        $this->getLayout()->getBlock('head')->setTitle($this->__("View Logs"));
+        $this->renderLayout();
+    }
+
+    public function configurationAction()
+    {
+        $this->loadLayout();
+        $this->getLayout()->getBlock('head')->setTitle($this->__("Logs Configuration"));
         $this->renderLayout();
     }
 }
